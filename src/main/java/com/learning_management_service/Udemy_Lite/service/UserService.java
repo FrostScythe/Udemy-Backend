6c10bb0873aa5f54b787f485a14f42d691fa7eb0
FrostScythe <maheshwariayush371@gmail.com>
2025-08-21 18:22:31 +0530

    Readme Updated

--- src/main/java/com/learning_management_service/Udemy_Lite/service/UserService.java
+++ src/main/java/com/learning_management_service/Udemy_Lite/service/UserService.java
@@ -1,5 +1,6 @@
 package com.learning_management_service.Udemy_Lite.service;
 
+import com.learning_management_service.Udemy_Lite.model.User;
 import com.learning_management_service.Udemy_Lite.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,10 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     UserRepository userRepository;
+
+    public String addUser(User userRequest){
+    userRepository.save(userRequest);
+    return "User saved successfully";
+    }
 
 }
