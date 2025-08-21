96cab0d2f3fcdee6313df15dd35e84d74d409ed2
FrostScythe <maheshwariayush371@gmail.com>
2025-08-21 18:58:25 +0530

    User updated

--- src/main/java/com/learning_management_service/Udemy_Lite/controller/UserController.java
+++ src/main/java/com/learning_management_service/Udemy_Lite/controller/UserController.java
@@ -1,6 +1,22 @@
 package com.learning_management_service.Udemy_Lite.controller;
 
+import com.learning_management_service.Udemy_Lite.model.User;
+import com.learning_management_service.Udemy_Lite.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/udemy/user")
 public class UserController {
+    @Autowired
+    UserService userService;
 
-
+    @PostMapping("/save")
+    public String saveUser(@RequestBody User userRequest){
+        String response =userService.addUser(userRequest);
+        return response;
+    }
 }
--- src/main/java/com/learning_management_service/Udemy_Lite/service/UserService.java
+++ src/main/java/com/learning_management_service/Udemy_Lite/service/UserService.java
@@ -5,6 +5,8 @@ import com.learning_management_service.Udemy_Lite.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 
 public class UserService {
@@ -13,7 +15,32 @@ public class UserService {
 
     public String addUser(User userRequest){
     userRepository.save(userRequest);
-    return "User saved successfully";
+    return "User saved successfully !!";
+    }
+
+    public User getUserById(int id){
+        Optional<User> userOptional= userRepository.findById(id);
+        if(userOptional.isPresent()){
+            return userOptional.get();
+        }else{
+            return null;
+        }
+    }
+
+    public String updateUser(int id, User newUserRequest){
+        User existingUser = getUserById(id);
+        if(existingUser != null){
+            newUserRequest.setId(id); // Set the ID explicitly
+            userRepository.save(newUserRequest);
+            return "User updated successfully";
+        } else {
+            return "User not found, hence cannot be updated";
+        }
+    }
+
+    public String deleteUserbyId(int id){
+        userRepository.deleteById(id);
+        return "UserStudent with id: " +id+" got deleted successfully!";
     }
 
 }
